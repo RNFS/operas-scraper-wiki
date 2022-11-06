d24166dae3be4a1e983bef68488182278965a559
Radwan Faris <radwanfaris13@gmail.com>
2022-11-06 22:46:29 +0200

    stable version of both spiders

--- src/spiders/wiki_comp_scraper.py
+++ src/spiders/wiki_comp_scraper.py
@@ -1,7 +1,9 @@
 import  scrapy
 import time 
 import re
+import xml.etree.ElementTree as ET
 
+ 
 class ComposersScraper(scrapy.Spider):
     name = "wikicomp"
 
@@ -15,7 +17,8 @@ class ComposersScraper(scrapy.Spider):
     
     # get all nationalities links
     def nat_parser(self, response):
-        nationalities =  response.xpath("//div[@id='mw-subcategories']//span[@class='CategoryTreeEmptyBullet']/following-sibling::a//@href").getall()
+        nationalities = response.xpath("//div[@id='mw-subcategories']//span[@class='CategoryTreeBullet']/following-sibling::a//@href | //div[@id='mw-subcategories']//span[@class='CategoryTreeEmptyBullet']/following-sibling::a//@href").getall()
+        # nationalities =  response.xpath("//div[@id='mw-subcategories']//span[@class='CategoryTreeEmptyBullet']/following-sibling::a//@href").getall()
         domain = "https://en.wikipedia.org/"
         for nat in nationalities:
             if country := re.search(r"^.*:([a-zA-Z]*)_.*$", nat):
@@ -24,9 +27,10 @@ class ComposersScraper(scrapy.Spider):
                 country = "None"
 
             nat_url = domain + nat 
-            print(country)
+
             yield scrapy.Request(url=nat_url, callback=self.pages_parser, meta={"country":country})
             time.sleep(5)
+
 
     def pages_parser(self, response):
         counrty = response.meta.get("country")
@@ -46,8 +50,6 @@ class ComposersScraper(scrapy.Spider):
         if sub_categories := response.xpath("//div[@id='mw-subcategories']//a/@href").getall():
             for category in sub_categories:
                 category_link = domain + category
-                print(category_link)
-                print("#" * 30)
                 yield scrapy.Request(url=category_link, callback=self.pages_parser, meta={"country":counrty})
                 time.sleep(5)
                     
@@ -56,33 +58,105 @@ class ComposersScraper(scrapy.Spider):
         name = response.xpath("//span[@class='mw-page-title-main']/text()").get()
         era = None 
         year = None
-        country = response.meta.get("country")
-        # extract era from birthday if birth is avai 
-        if birthdate := response.xpath("//table[@class='infobox vcard plainlist']//*[text()[contains(., 'Born')]]//following-sibling::td/text()").getall():
-            birthdate= "".join(birthdate)
-            year = re.search(r"(\d{4})", birthdate)
-            if year:
-                year = year.group(1)
-                era = f"{year[:2]}th" 
+        birthdate = None
+        style = None 
+        date_of_death = None
+        biography  = None  
 
-        else :
-            if birthdate := response.xpath("//*[text()[contains(., 'Born')]]//following-sibling::td/text()").getall():
-                birthdate= "".join(birthdate)
+        country = response.meta.get("country")
+        if country == "New" :
+            country = "New Zealand"
+        elif country == "Opera":
+            country = None            
+
+        # extract era from birthday if birth is avai in different ways 
+        birthdate_f= response.xpath("//*[text()[contains(., 'Born')]]//following-sibling::td/text()").getall()
+        birthdate_s =response.xpath("//table[@class='infobox biography vcard']//*[text()[contains(., 'Born')]]//following-sibling::td/text()").getall()
+        birthdate_t = response.xpath("//table[@class='infobox vcard plainlist']//*[text()[contains(., 'Born')]]//following-sibling::td/text()").getall()            
+            
+        if birthdate_f or birthdate_s or birthdate_t:  
+            if birthdate := list(self.value_checker([birthdate_f, birthdate_s, birthdate_t])):
+                birthdate= "".join(str(x) for x  in birthdate)
+                birthdate ="".join(birthdate)
+                birthdate = birthdate.replace("[", "")
+                birthdate = birthdate.replace("]", "")
+                birthdate = birthdate.replace(",", "")
+                birthdate = birthdate.replace("'", "")
+                
+
                 year = re.search(r"(\d{4})", birthdate)
                 if year:
                     year = year.group(1)
-                    era = f"{year[:2]}th"
-        if date_of_death :=  response.xpath("//*[text()[contains(., 'Died')]]//following-sibling::td/text()").get():
-            date_of_death = "".join(date_of_death)
-    
-        biography = response.xpath("//div[@class='mw-parser-output']/p/text()").getall()
-        biography = "".join(biography)
+                    era = f"{year[:2]}th" 
 
-        yield {
-            "name": name,
-            "birthdate": birthdate,
-            "era" : era, 
-            "country":country, 
-            "date death": date_of_death,
-            "biography": biography,
-            }
+        date_of_death_f = response.xpath("//table[@class='infobox biography vcard']//*[text()[contains(., 'Died')]]//following-sibling::td/text()").getall()
+        date_of_death_s = response.xpath("//table[@class='infobox vcard plainlist']//*[text()[contains(., 'Died')]]//following-sibling::td/text()").getall() 
+        date_of_death_t =  response.xpath("//*[text()[contains(., 'Died')]]//following-sibling::td/text()").get()
+        if date_of_death_f or date_of_death_s or date_of_death_t:
+            date_of_death = list(self.value_checker([date_of_death_f, date_of_death_s, date_of_death_t])) 
+            
+            date_of_death = "".join(str(x) for x  in  date_of_death)
+            date_of_death = date_of_death.replace("[", "")
+            date_of_death = date_of_death.replace("]", "")
+            date_of_death = date_of_death.replace(",", "")
+            date_of_death = date_of_death.replace("'", "")
+            
+
+        style_a = response.xpath("//table[@class='infobox biography vcard']//*[text()[contains(., 'Style')]]//following-sibling::td/text()").getall()
+        style_b = response.xpath("//table[@class='infobox vcard plainlist']//*[text()[contains(., 'Style')]]//following-sibling::td/text()").getall() 
+        style_c =  response.xpath("//*[text()[contains(., 'Style')]]//following-sibling::td/text()").getall()
+
+        style_d = response.xpath("//table[@class='infobox biography vcard']//*[text()[contains(., 'Genres')]]//following-sibling::td/text()").getall()
+        style_e = response.xpath("//table[@class='infobox vcard plainlist']//*[text()[contains(., 'Genres')]]//following-sibling::td/text()").getall() 
+        style_f =  response.xpath("//*[text()[contains(., 'Genres')]]//following-sibling::td/text()").getall()
+        
+        if style_a or style_b or style_c or style_d or  style_e or style_f :
+            style = list(*self.value_checker([style_a , style_b , style_c, style_d, style_e, style_f])) 
+            style = "".join("".join(str(x) for x in style))
+            style = "".join(style)
+            style = style.replace(",", "")
+            if not style:
+                style = None
+
+
+        if biography := response.xpath("//div[@class='mw-parser-output']/p/text()").getall():
+            biography = "".join(biography)
+        
+        # writing as xml file 
+        comp_ = ET.Element('composer')
+        e_name = ET.SubElement(comp_, "name")
+        e_era = ET.SubElement(comp_, "era")
+        e_style = ET.SubElement(comp_, "style")
+        e_birthdate = ET.SubElement(comp_, "birthdate")
+        e_country = ET.SubElement(comp_, "nationality")
+        e_date_of_death = ET.SubElement(comp_,"death")
+        e_biography = ET.SubElement(comp_,"biography")
+
+        e_name.text = name
+        e_birthdate.text = birthdate
+        e_era.text = era
+        e_country.text = country
+        e_style.text = style
+        e_date_of_death.text = date_of_death
+        e_biography.text = biography  
+        
+        b_xml = ET.tostring(comp_)
+        with open("composers.xml", "ab") as f:
+            f.write(b_xml)
+
+
+        # yield {
+        #     "name": name,
+        #     "birthdate": birthdate,
+        #     "era" : era, 
+        #     "country":country, 
+        #     "date death": date_of_death,
+        #     "biography": biography,
+        #     }
+
+    def value_checker(self, args):
+        for x in args:
+            if x :
+                yield x 
+                break
+            
--- src/spiders/wiki_scraper.py
+++ src/spiders/wiki_scraper.py
@@ -1,19 +1,22 @@
 import scrapy 
 import time
 import re
+import xml.etree.ElementTree as ET
+
+
 class OperasScraper(scrapy.Spider):
     name = "wiki_operas"
 
     # loop over the years by adding 10 years every time and wiki will get us all the years avai in this decade
     def start_requests(self):
-        decade = 1700
-        while decade <= 1710:
+        decade = 1590
+        while decade <= 2023:
             urls = [
                 f"https://en.wikipedia.org/wiki/Category:{decade}s_operas",
                 ] 
             for url in urls:
                 yield scrapy.Request(url=url, callback=self.de_parser)
-                time.sleep(4)
+                time.sleep(5)
                 print(decade)
             decade += 10
     
@@ -21,7 +24,6 @@ class OperasScraper(scrapy.Spider):
     def de_parser(self, response):
         list_years = response.xpath("//div[@class='mw-category-group']//a/@href").getall()
         domain = "https://en.wikipedia.org/"
-        print(list_years)
         for next_year in list_years:
             if year := re.search(r"^.*:(\d{4}).*$",next_year):
                 print(year)
@@ -56,19 +58,49 @@ class OperasScraper(scrapy.Spider):
         date = response.xpath("//div[@class='vevent']/text()").get()
         place = response.xpath("//div[@class='summary']//text()").getall()
         place = "".join(place)   
-        summry  =  response.xpath("//table[@class='infobox vevent']/following-sibling::p/text()").getall()   
-        summary = "".join(summry)   
+        summray  =  response.xpath("//table[@class='infobox vevent']/following-sibling::p/text()").getall()   
+        summary = "".join(summray)   
         librettist =response.xpath("//table[@class='infobox vevent']//*[text()[contains(.,'Librettist')]]/following-sibling::td//text()").get() 
-        yield {
+        language =  response.xpath("//table[@class='infobox vevent']//*[text()[contains(.,'Language')]]/following-sibling::td/text()").get()
+
+
+        ## writing as xml foramt
+        opera_ = ET.Element('opera')
+        e_name = ET.SubElement(opera_, "name")
+        e_composers = ET.SubElement(opera_, "composers")
+        e_date = ET.SubElement(opera_, "premieredate")
+        e_place = ET.SubElement(opera_, "premiereplace")
+        e_language= ET.SubElement(opera_,"language")
+        e_era = ET.SubElement(opera_,"era")
+        e_librettist =  ET.SubElement(opera_,"librettist")  
+        e_year = ET.SubElement(opera_,"year")
+        e_summary = ET.SubElement(opera_,"summary")
+
+        e_name.text = name
+        e_composers.text = composers
+        e_era.text = era
+        e_place.text = place
+        e_date.text = date
+        e_language.text =  language
+        e_librettist.text = librettist  
+        e_year.text = year
+        e_summary.text = summary
+
+        b__xml = ET.tostring(opera_)
+        with open("operas.xml", "ab") as f:
+            f.write(b__xml)
+
+
+        # yield {
             
-            "name": name,
-            "composers": composers,
-            "premiere_date": date,
-            "premiere_place": place,
-            "Language" : response.xpath("//table[@class='infobox vevent']//*[text()[contains(.,'Language')]]/following-sibling::td/text()").get(),
-            "era" : f"{era}th",
-            "librettist": librettist,
-            "year": year, 
-            "summary": summary 
-        }
+        #     "name": name,
+        #     "composers": composers,
+        #     "premiere_date": date,
+        #     "premiere_place": place,
+        #     "Language" : language,
+        #     "era" : f"{era}th",
+        #     "librettist": librettist,
+        #     "year": year, 
+        #     "summary": summary 
+        # }
 
